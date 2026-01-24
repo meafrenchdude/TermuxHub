02445c00f07a2ec27275ed40123b2552d51b26ea
M A A Z <104779758+maazm7d@users.noreply.github.com>
2026-01-24 15:34:33 +0530

    v2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.maazm7d.termuxhub"
         minSdk = 25
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
